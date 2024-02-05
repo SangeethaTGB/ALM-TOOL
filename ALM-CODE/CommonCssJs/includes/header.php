5942cf36ab71cd98b26f8041bc1b20736413c13f
sangeetha <smit2@tgbhyd.in>
2024-02-05 13:46:58 +0530

    Set pid and remove other includes
    Initialized $pid and commented other include files - depnocss.php & phpfunctions.php

--- ALM-CODE/CommonCssJs/includes/header.php
+++ ALM-CODE/CommonCssJs/includes/header.php
@@ -17,7 +17,7 @@
               </div>
             <!--logo start-->
             <div class="top-menu" style="display: flex; justify-content: space-around; ">
-              <a><b><?php include $_SERVER['DOCUMENT_ROOT'] . '\mis\depNoCSS.php'; ?></b></a> 
+              <a><b><?php $pid=99999;// include $_SERVER['DOCUMENT_ROOT'] . '\mis\depNoCSS.php'; ?></b></a> 
               <a href="/mis/Departments/HeadOffice/ALM/" style="color: white"><h1>Asset and Liability Management (ALM)</h1></a>               
               <div style="float: right;display: flex;  ">             
                   <h5 class="centered"><?php echo "$EmpName ($pid)"; ?><br><?php echo "$branchname ($EmpBrcode)"; ?></h5>
@@ -27,7 +27,7 @@
                     <li><a class="logout" href="logout.php">Logout</a></li>
               </ul-->
             </div>
-              <?php include $_SERVER['DOCUMENT_ROOT'] . '\mis\phpFunctions.php'; ?> 
+              <?php //include $_SERVER['DOCUMENT_ROOT'] . '\mis\phpFunctions.php'; ?> 
               <?php include 'CommonCssJs\includes\number_to_text.php'; ?> 
 
        </header>
